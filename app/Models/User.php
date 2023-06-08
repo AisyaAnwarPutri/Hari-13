5e47a349d330a5bbb8828162117bf13c2fc53d81
AisyaAnwarPutri <aisyanwarputri@gmail.com>
2023-06-08 10:17:01 +0700

    model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function kritiks()
+    {
+        return $this->hasMany(Kritik::class);
+    }
 }
